96a0d10894f3bea4268880140adb8eb8f3d58eca
Trishant Pahwa <trishantpahwa@gmail.com>
2020-08-09 14:35:43 +0530

    Added BothDirectionRelationship test between 2 Nodes.

--- test/Node.test.js
+++ test/Node.test.js
@@ -19,7 +19,7 @@ describe('Testing Node constructors', function() {
             'relationships': []
         });
     });
-    it('Should create a Node with a Relationship', function() {
+    it('Should create a Node with a basic uni-direction(Single) Relationship with another Node', function() {
         let node = new Node('Student', {'Firstname': 'Trishant', 'Lastname': 'Pahwa'}, []);
         let node2 = new Node('Subject', {'Name': 'Computer Science'}, []);
         let relationship1 = new SingleDirectionRelationship('Studies', {'Since': '2014'}, node2);
@@ -66,9 +66,75 @@ describe('Testing Node constructors', function() {
             }
         });
     });
-    // let relationship2 = new BothDirectionRelationship('Studies', {'Since': '2014'}, [node1, node2]);
-    // node1.addRelationship(relationship2);
-    // node2.addRelationship(relationship2)
-    // console.log(node1);
-    // console.log(node2);
+    it('Should create Node and a basic bi-directional(Both) Relationship with another Node', function() {
+        let node1 = new Node('Person', {'Firstname': 'Trishant', 'Lastname': 'Pahwa'}, []);
+        let node2 = new Node('Person', {'Firstname': 'Shantanu', 'Lastname': 'Saini'}, []);
+        let relationship = new BothDirectionRelationship('Friends', {'Type': 'Best Friends', 'Since': '2000'}, [node1, node2]);
+        node1.addRelationship(relationship);
+        node2.addRelationship(relationship);
+        let node1ID = node1.getID();
+        let node2ID = node2.getID();
+        let relationshipID = relationship.getID();
+        assert.deepEqual({'Node1': node1, 'Node2': node2, 'Relationship': relationship}, {
+            'Node1': {
+                'id': node1ID,
+                'name': 'Person',
+                'properties': {
+                    'Firstname': 'Trishant',
+                    'Lastname': 'Pahwa'
+                },
+                relationships: [
+                    {
+                        'direction': 'both',
+                        'id': relationshipID,
+                        'name': 'Friends',
+                        'properties': {
+                            'Type': 'Best Friends',
+                            'Since': '2000'
+                        },
+                        'realtedNode': {
+                            'left': node1ID,
+                            'right': node2ID
+                        }
+                    }
+                ]
+            },
+            'Node2': {
+                id: node2ID,
+                'name': 'Person',
+                'properties': {
+                    'Firstname': 'Shantanu',
+                    'Lastname': 'Saini'
+                },
+                relationships: [
+                    {
+                        'direction': 'both',
+                        'id': relationshipID,
+                        'name': 'Friends',
+                        'properties': {
+                            'Type': 'Best Friends',
+                            'Since': '2000'
+                        },
+                        'realtedNode': {
+                            'left': node1ID,
+                            'right': node2ID
+                        }
+                    }
+                ]
+            },
+            'Relationship': {
+                'direction': 'both',
+                'id': relationshipID,
+                'name': 'Friends',
+                'properties': {
+                    'Type': 'Best Friends',
+                    'Since': '2000'
+                },
+                'realtedNode': {
+                    'left': node1ID,
+                    'right': node2ID
+                }
+            }
+        })
+    });
 });
