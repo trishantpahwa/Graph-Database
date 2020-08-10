47cd1c20c4675906f1fc6f84fefb65e70024aed1
Trishant Pahwa <trishantpahwa@gmail.com>
2020-08-10 16:50:35 +0530

    Added feature to read from database.

--- src/Database.js
+++ src/Database.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { assert } = require('console');
 
 class Database {
 
@@ -10,7 +11,7 @@ class Database {
         this.mapName = dbName + '.map';
     }
 
-    addObjectToDatabase(data) {
+    addNodeToDatabase(data) {
         try {
             if(typeof data !== 'object') {
             throw 'Object is not of type JSON';
@@ -32,15 +33,16 @@ class Database {
         if(mapSize > 4) {
             fs.truncateSync(this.mapName, mapSize-2);
             dataToWrite = ',' + JSON.stringify({
-                [nodeID]: {
+                ['Node-' + nodeID]: {
                     'start': start.toString(),
                     'end': end.toString()
                 }
             }, null, 4).slice(1, -1) + '}';
         } else {
+            end -= 1;
             fs.truncateSync(this.mapName, mapSize-3);
             dataToWrite = JSON.stringify({
-                [nodeID]: {
+                ['Node-' + nodeID]: {
                     'start': start.toString(),
                     'end': end.toString()
                 }
@@ -55,7 +57,85 @@ class Database {
         var data = fs.readFileSync(this.mapName);
         data = JSON.parse(data);
         return Object.keys(data).length;
+    }  // Need function to count nodes and relationships seperately
+    readNodefromDatabase(identifier) {
+        if(typeof identifier === 'string') {
+            return this.getNodeFromID(identifier).then(function(node) {
+                return node;
+            }).catch(function(err) {
+                console.log(err);
+                return ;
+            });
+        }
+        if(typeof identifier === 'object') {
+            return this.getNodeFromProperties(identifier).then(function(node) {
+                return node;
+            }).catch(function(err) {
+                console.log(err);
+                return ;
+            })
+        }
+    }
+    getNodeFromID(id) {
+        id = 'Node-' + id;
+        let _dbName = this.dbName;
+        let _mapName = this.mapName;
+        return new Promise(function(resolve, reject) {
+            let map = JSON.parse(fs.readFileSync(_mapName));
+            if(Object.keys(map).indexOf(id) <= -1) {
+                reject('ID ' + id + ' not found');
+            }
+            let start = parseInt(map[id].start);
+            let end = parseInt(map[id].end);
+            var stream = fs.createReadStream(_dbName, {start: start, end: end});
+            let object = null;
+            stream.on('data', function(chunk) {
+                object = JSON.parse('{' + chunk.toString() + '}');
+            });
+            stream.on('close', function() {
+                if(object == null) {
+                    reject('Couldn\'t read object');
+                } else {
+                    resolve(object);
+                }
+            });
+        });
+    }
+    getNodeFromProperties (properties) {
+        let _dbName = this.dbName;
+        let _mapName = this.mapName;
+        var _regex = /^Node-\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b$/gm;
+        return new Promise(async function(resolve, reject) {
+            let nodes = [];
+            let map = JSON.parse(fs.readFileSync(_mapName));
+            Object.keys(map).forEach(function(key) {
+                if(!key.match(_regex)) {
+                    delete map[key];
+                }
+            });
+            for(let node in map) {
+                let start = parseInt(map[node].start);
+                let end = parseInt(map[node].end);
+                let nodeData = null;
+                let maxMatch = Object.keys(properties).length;
+                for await (const chunk of fs.createReadStream(_dbName, {start: start, end: end})) {
+                    let match = 0;
+                    nodeData = JSON.parse('{' + chunk.toString() + '}');
+                    nodeData = nodeData[Object.keys(nodeData)[0]];
+                    for(var key in properties) {
+                        if(nodeData.properties[key] == properties[key]) {
+                            match += 1;
+                        }
+                    }
+                    if(match == maxMatch) {
+                        nodes.push(nodeData);
+                    }
+                }
+            }
+            resolve(nodes);
+        });
     }
 }
+
 
 module.exports = Database;
--- test/Database.test.js
+++ test/Database.test.js
@@ -11,11 +11,11 @@ describe('Interaction with the database.', function() {
         fs.writeFileSync(dbName + '.map', '{\n\n}');
         let database = new Database(dbName);
         let node = new Node('Student', {'Firstname': 'Trishant', 'Lastname': 'Pahwa'}, []);
-        database.addObjectToDatabase(node.toJSON());
+        database.addNodeToDatabase(node.toJSON());
         let writtenData = fs.readFileSync(dbName + '.map').toString();
         writtenData = JSON.parse(writtenData);
-        let start = parseInt(writtenData[node.getID()].start);
-        let end = parseInt(writtenData[node.getID()].end);
+        let start = parseInt(writtenData['Node-' + node.getID()].start);
+        let end = parseInt(writtenData['Node-' + node.getID()].end);
         var stream = fs.createReadStream(dbName + '.graphdb', {start: start, end: end});
         stream.on('data', function(chunk) {
             let _n = JSON.parse('{' + chunk.toString() + '}');
@@ -25,12 +25,12 @@ describe('Interaction with the database.', function() {
     it('Should add a node to a non-empty database.', function() {
         const dbName = 'mydatabase';
         let database = new Database(dbName);
-        let node = new Node('Student', {'Firstname': 'Trishant', 'Lastname': 'Pahwa'}, []);
-        database.addObjectToDatabase(node.toJSON());
+        let node = new Node('Subject', {'Name': 'Arnav', 'Cost': '200'}, []);
+        database.addNodeToDatabase(node.toJSON());
         let writtenData = fs.readFileSync(dbName + '.map').toString();
         writtenData = JSON.parse(writtenData);
-        let start = parseInt(writtenData[node.getID()].start);
-        let end = parseInt(writtenData[node.getID()].end);
+        let start = parseInt(writtenData['Node-' + node.getID()].start);
+        let end = parseInt(writtenData['Node-' + node.getID()].end);
         var stream = fs.createReadStream(dbName + '.graphdb', {start: start, end: end});
         stream.on('data', function(chunk) {
             let _n = JSON.parse('{' + chunk.toString() + '}');
@@ -43,4 +43,33 @@ describe('Interaction with the database.', function() {
         var objects = database.countDatabaseObjects();
         assert.equal(objects, 2);
     });
+    it('Should be able to read nodes from database using ID', function() {
+        let dbName = 'mydatabase'
+        let database = new Database(dbName);
+        let node = new Node('Student', {'Firstname': 'Tavish', 'Lastname': 'Pahwa'}, []);
+        database.addNodeToDatabase(node.toJSON());
+        database.readNodefromDatabase(node.getID()).then(function(_n) {
+            assert.deepEqual(_n, node.toJSON());
+        });
+    });
+    it('Should be able to read nodes from database using property filter', function() {
+        let dbName = 'mydatabase';
+        let database = new Database(dbName);
+        let property = {'Lastname': 'Pahwa'};
+        let node = new Node('Student', {'Firstname': 'Trishant', 'Lastname': 'Pahwa'}, []);
+        node = node.toJSON();
+        node = node[Object.keys(node)[0]];
+        delete node.id;
+        let node2 = new Node('Student', {'Firstname': 'Tavish', 'Lastname': 'Pahwa'}, []);
+        node2 = node2.toJSON();
+        node2 = node2[Object.keys(node2)[0]];
+        delete node2.id;
+        var nodes = [node, node2];
+        database.readNodefromDatabase(property).then(function(_n) {
+            _n.forEach(function(n) {
+                delete n.id;
+            });
+            assert.deepEqual(nodes, _n);
+        });
+    })
 });
